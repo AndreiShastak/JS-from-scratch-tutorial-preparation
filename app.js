1b94754bd2980193b25cc95c1b6949867cf4891f
AndreiShastak <Andrei.tak@gmail.com>
2021-07-04 17:32:14 +0200

    First function

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 
-
-const lastName ='Shastak'
-let age=26
-const isProgrammer=true
-
-age ='Andr'
-
-console.log(name)
+const name ='Shastak';
+let age=26;
+const isProgrammer=true;
+const lastName=prompt('wwedite familiju')
+let year=prompt('wwedite god rozdenija')
+//console.log('familija: '+lastName+' a wozrast:'+ age)
+function calculateAge(year){
+    return 2021-year
+}
+function logInfoAbout(lastName,year) {
+    const age= calculateAge(year)
+    console.log('czelowek po imeni '+lastName+' imeet wozrast '+ age)
+}
+logInfoAbout(lastName,year);
