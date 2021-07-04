a83a23d250221c7f79b99f3355cc7a2ff8181f9e
AndreiShastak <andrei.tak@gmail.com>
2021-07-04 19:32:12 +0200

    Add: string-tutorial

---
+++ string.js
@@ -0,0 +1,48 @@
+// const name = 'Anton'
+// const age = 26
+//
+// function getAge() {
+//   return age
+// }
+
+// const output = 'Привет, меня зовут ' + name + ' и мой возраст ' + age + ' лет.'
+// const output = `Привет, меня зовут ${name} и мой возраст ${age < 30 ? 'A' : 'B'} лет.`
+// const output = `
+//   <div>This is div</div>
+//   <p>this is o</p>
+// `
+
+// console.log(output)
+
+// const name = 'Anton'
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.charAt(2))
+// console.log(name.indexOf('!'))
+// console.log(name.toLowerCase().startsWith('An'))
+// console.log(name.startsWith('ANT'))
+// console.log(name.endsWith('on'))
+// console.log(name.repeat(3))
+// const string = '     password      '
+// console.log(string.trim())
+// console.log(string.trimLeft())
+// console.log(string.trimRight())
+
+function logPerson(s, name, age) {
+    if (age < 0) {
+        age = 'Еще не родился'
+    }
+    return `${s[0]}${name}${s[1]}${age}${s[2]}`
+}
+
+const personName = 'Anton'
+const personName2 = 'Andrei'
+const personAge = 26
+const personAge2 = -10
+
+const output = logPerson`Имя: ${personName}, Возраст: ${personAge}!`
+const output2 = logPerson`Имя: ${personName2}, Возраст: ${personAge2}!`
+
+console.log(output)
+console.log(output2)
