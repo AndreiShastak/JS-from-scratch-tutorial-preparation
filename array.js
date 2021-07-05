1993bd8cd5628e0c671e0224137a471e73defc3a
AndreiShastak <Andrei.tak@gmail.com>
2021-07-05 17:12:34 +0200

    Add: array-tutorial-update

--- array.js
+++ array.js
@@ -11,15 +11,15 @@ function addItemToEnd() {
 
 }
 
-// Method
-// cars.push('Рено')
-// cars.unshift('Волга')
-//
-// const firstCar = cars.shift()
-// const lastCar = cars.pop()
-// console.log(firstCar)
-// console.log(lastCar)
-// console.log(cars)
+Method
+cars.push('Рено')
+cars.unshift('Волга')
+
+const firstCar = cars.shift()
+const lastCar = cars.pop()
+console.log(firstCar)
+console.log(lastCar)
+console.log(cars)
 
 // console.log(cars.reverse())
 // console.log(cars)
